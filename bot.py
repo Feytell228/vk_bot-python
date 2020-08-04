345b1e6b92189a9ca85f32a23c744856af5d06aa
FEYTELL's CHANNEL <robloxgeroy09@gmail.com>
2020-08-04 13:22:36 +0000

    bot version 2.0 -updated code -add new function write_msg() p.s beta version

--- bot.py
+++ bot.py
@@ -6,29 +6,26 @@ import vk_api
 from vk_api.longpoll import VkLongPoll, VkEventType
 
 
-vk_session=vk_api.VkApi(token="30b1235ef7a47f25146523bb6fd174e8f63438e7169860e5caf7e0505083a320da07b84e6629392c355d3") 
+token="30b1235ef7a47f25146523bb6fd174e8f63438e7169860e5caf7e0505083a320da07b84e6629392c355d3"
+
+vk=vk_api.VkApi(token=token)
+
+longpoll=VkLongPoll(vk)
 
 text=["Кек","Лол","Ну ты и лох","bot by Xachapury","Бтс топ","порно асманской империи "," Чукча","водка плоха","Привет","Здорова", "хеллоу", "хелло","хай","зиг хаиль","как дела","че каво","пошол нахуй","как тебя зовут", "ты че пидр",]
 
 running=True
 
-longpoll=VkLongPoll(vk_session)
-vk=vk_session.get_api()
-#листание лонга
-for event in longpoll.listen():
+def write_msg(message, user_id, random_id):
+  vk.method('messages.send', {'message': message,'user_id': user_id,'random_id': random_id})
   
-  if event.type==VkEventType.MESSAGE_NEW and event.to_me and event.text:
-    if event.text=="!даун":
-      if event.from_chat:
-        vk.messages.send(
-        chat_id=event.chat_id,
-        random_id=random.randomint(64),
-        message=random.choice(text))
-    elif event.text=="!хелп",or event.text=="!помощь":
-      elif event.from_chat:
-        vk.messages.send(
-          chat_id=event.chat_id,
-          random_id=random.randomint(64),
-          message="Команды: !даун"
-          )
-        
+for event in longpoll.listen():
+    if event.type==VkEventType.MESSAGE_NEW:
+      if event.to_me:
+        request=event.text
+        if request=='TEST':
+          print('получено')
+          write_msg('слышу', event.user_id, random.getrandbits(64))
+        else:
+          write_msg('?', event.user_id, random.getrandbits(64))
+          
