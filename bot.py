57ad40a82cbe2bea0e8929ff323a9f2846479936
FEYTELL's CHANNEL <robloxgeroy09@gmail.com>
2020-08-06 21:28:56 +0000

    Bot version 3.0 new commands

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ import vk_api
 from vk_api.longpoll import VkLongPoll, VkEventType
 
 
-token="30b1235ef7a47f25146523bb6fd174e8f63438e7169860e5caf7e0505083a320da07b84e6629392c355d3"
+token="00b1235ef7a47f25146523bb6fd174e8f63438e7169860e5caf7e0505083a320da07b84e6629392c355d4"
 
 vk=vk_api.VkApi(token=token)
 
@@ -23,6 +23,15 @@ for event in longpoll.listen():
     if event.type==VkEventType.MESSAGE_NEW:
       request=event.text
       if event.from_user:
-        if request=='TEST':
-          print('получено')
-          write_msg(random.choice(text), event.user_id, random.getrandbits(64))
+        if request=='!даун':
+          print('получено !даун')
+          
+          write_msg(random.choice(text), event.user_id, random.getrandbits(64))  #это полностью копируй
+        elif request=='!хелп':
+          print('получено !хелп')
+          write_msg('в разработке', event.user_id, random.getrandbits(64))
+        elif request=='!вики':
+          print('получено !вики')
+          write_msg('в разработке', event.user_id, random.getrandbits(64))
+          #write_msg('каво', event.user_id, random.getrandbits(64))
+        
